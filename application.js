c788e5e46d0b63fc0823a4f4b2be7156ffa3dac6
jilucev <jilucev7@gmail.com>
2014-04-10 02:07:21 +0000

    first commit

---
+++ application.js
@@ -0,0 +1,25 @@
+$(function(){
+
+  $('#magic-button').on('click', loadLighthouses);
+
+  function loadLighthouses() {
+    var url = "https://api.instagram.com/v1/tags/lighthouse/media/recent?client_id=8016d14255074769a95cf9212ada71a1&callback=?";
+    $.ajax(url, {
+      dataType: "jsonp",
+      success: function(data) {
+        console.log(data);
+          for(i= 0; i < data.data.length; i++) {
+            var lighthouse = data.data[i];
+            $('<img src=" ' + lighthouse.images.low_resolution.url + ' "/> ').appendTo($('body'));
+          }
+
+        },
+        error: function() {
+          alert('Error!')
+        }
+    });
+  }
+
+});
+
+
