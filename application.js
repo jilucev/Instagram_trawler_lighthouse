1581647b95df900935fb2847ba7d39210a85afb0
jilucev <jilucev7@gmail.com>
2014-04-10 21:30:15 +0000

    hover looks cool

--- application.js
+++ application.js
@@ -1,7 +1,12 @@
 $(function(){
 
   $('#magic-button').on('click', loadLighthouses);
+  $('#magic-button').on('click', hideMagicButton);
+  
+
+  $('#spring-cleaning').hide();
   $('#spring-cleaning').on('click', clearLighthouses);
+  $('#spring-cleaning').on('click', hideSpringCleaningButton);
 
   function loadLighthouses() {
     var url = "https://api.instagram.com/v1/tags/lighthouse/media/recent?client_id=8016d14255074769a95cf9212ada71a1&callback=?";
@@ -11,7 +16,7 @@ $(function(){
         console.log(data);
           for(i= 0; i < data.data.length; i++) {
             var lighthouse = data.data[i];
-            $('<img src=" ' + lighthouse.images.low_resolution.url + ' " class="image"/> ').hide().appendTo($('body')).fadeIn(i * 600);
+            $('<img src=" ' + lighthouse.images.low_resolution.url + ' " class="image"/> ').hide().appendTo($('body')).fadeIn(i * 300);
           }
 
         },
@@ -26,6 +31,15 @@ $(function(){
     $('.image').fadeOut(i * 300);
   }
 
+  function hideMagicButton() {
+    $('#magic-button').hide();
+    $('#spring-cleaning').show();
+  }
+
+  function hideSpringCleaningButton() {
+    $('#spring-cleaning').hide();
+    $('#magic-button').show();
+  }
 
 });
 
