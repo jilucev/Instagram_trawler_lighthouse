03f9c3edac3de9682a6ea8c2a7fb474ced4c4d49
jilucev <jilucev7@gmail.com>
2014-04-11 01:49:34 +0000

    update on interval timer

--- application.js
+++ application.js
@@ -1,22 +1,34 @@
 $(function(){
 
+  // setInterval();{loadLighthouses()}, 1000});
+
+function updateImages() {
+  setInterval(function() {
+    loadLighthouses();
+  }, 10000);
+}
+
   $('#magic-button').on('click', loadLighthouses);
   $('#magic-button').on('click', hideMagicButton);
+
+
+  $('#update-button').on('click', updateImages);
   
 
   $('#spring-cleaning').hide();
   $('#spring-cleaning').on('click', clearLighthouses);
   $('#spring-cleaning').on('click', hideSpringCleaningButton);
 
+  
+
   function loadLighthouses() {
     var url = "https://api.instagram.com/v1/tags/lighthouse/media/recent?client_id=8016d14255074769a95cf9212ada71a1&callback=?";
     $.ajax(url, {
       dataType: "jsonp",
       success: function(data) {
-        console.log(data);
           for(i= 0; i < data.data.length; i++) {
             var lighthouse = data.data[i];
-            $('<img src=" ' + lighthouse.images.low_resolution.url + ' " class="image"/> ').hide().appendTo($('body')).fadeIn(i * 300);
+            $('<img src=" ' + lighthouse.images.low_resolution.url + ' " class="image"/> ').hide().appendTo($('#wall')).fadeIn(1200);
           }
 
         },
@@ -28,7 +40,7 @@ $(function(){
 
   
   function clearLighthouses() {
-    $('.image').fadeOut(i * 300);
+    $('.image').fadeOut(1400);
   }
 
   function hideMagicButton() {
@@ -37,8 +49,8 @@ $(function(){
   }
 
   function hideSpringCleaningButton() {
-    $('#spring-cleaning').hide();
-    $('#magic-button').show();
+    $('#spring-cleaning').fadeOut(600);
+    $('#magic-button').fadeIn(600);
   }
 
 });
